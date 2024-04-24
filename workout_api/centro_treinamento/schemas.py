8bc8c2420242778ebb8cc3ddf056dc969d6929f9
Miguel de Freitas Muniz <miguimuniz@gmail.com>
2024-04-24 14:20:08 -0300

    feat: cria models e schemas para as entidades atleta, categoria e centro de treinamento

---
+++ workout_api/centro_treinamento/schemas.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+from pydantic import Field
+from workout_api.contrib.schemas import BaseSchema
+
+
+class CentroTreinamento(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT King', max_length=20)]
+    endereco: Annotated[str, Field(description='Endereço do centro de treinamento', example='Rua X, QD Y', max_length=60)]
+    proprietario: Annotated[str, Field(description='Proprietário do centro de treinamento', example='Pedro', max_length=30)]
