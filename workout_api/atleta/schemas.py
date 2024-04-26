57103572711b7d11e2c30531d1df640d965a3a43
Miguel de Freitas Muniz <miguimuniz@gmail.com>
2024-04-26 10:46:49 -0300

    feat: adiciona settings e inicia construção das rotas

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from pydantic import Field, PositiveFloat
-from workout_api.contrib.schemas import BaseSchema
+from workout_api.contrib.schemas import BaseSchema, OutMixin
 
 class Atleta(BaseSchema):
     nome: Annotated[str, Field(description='Nome do atleta', example='João', max_length=50)]
@@ -9,3 +9,8 @@ class Atleta(BaseSchema):
     peso: Annotated[PositiveFloat, Field(description='Peso do atleta', example=75.5)]
     altura: Annotated[PositiveFloat, Field(description='Altura do atleta', example=1.70)]
     sexo: Annotated[str, Field(description='Sexo do atleta', example='M', max_length=1)]
+    
+class AtletaIn(Atleta):
+    pass
+class AtletaOut(Atleta, OutMixin):
+    pass
