78d71c9e8602b1dff70786b84ae06e722217e173
Miguel de Freitas Muniz <miguimuniz@gmail.com>
2024-05-06 12:53:08 -0300

    feat: adiciona rotas dos centros de treinamento

--- workout_api/centro_treinamento/schemas.py
+++ workout_api/centro_treinamento/schemas.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from pydantic import Field
+from pydantic import UUID4, Field
 from workout_api.contrib.schemas import BaseSchema
 
 
@@ -7,3 +7,9 @@ class CentroTreinamentoIn(BaseSchema):
     nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT King', max_length=20)]
     endereco: Annotated[str, Field(description='Endereço do centro de treinamento', example='Rua X, QD Y', max_length=60)]
     proprietario: Annotated[str, Field(description='Proprietário do centro de treinamento', example='Pedro', max_length=30)]
+    
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[UUID4, Field(description='Identificador do centro de treinamento')]
+    
+class CentroTreinamentoAtleta(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT King', max_length=20)]
